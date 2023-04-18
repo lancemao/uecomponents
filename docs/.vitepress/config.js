09f14b4954cb9ceac44763dc55a07c5103d41f12
Lance\Mao <maolongdong@gmail.com>
2023-04-18 14:22:10 +0800

    modify base

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Unreal Components",
+  base: '/uecomponents/',
   description: "Simple & Easy components for Unreal Engine",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
